4022ea3c0e12514152d8455776eb886e9a65a072
Austin Rowsell <andyrow123@hotmail.co.uk>
2017-10-03 21:41:51 +0100

    Completed challenge 5. Tested top_up and added code to pass tests.

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -3,8 +3,16 @@ require './lib/oystercard'
 describe Oystercard do
 
   context '#balance' do
-    it 'has a balance of 0' do
+    it 'should initialize with a 0 balance' do
       expect(subject.balance).to eq 0
+    end
+  end
+
+  context '#top_up' do
+    it { is_expected.to respond_to(:top_up).with(1).argument }
+
+    it 'should add to balance' do
+      expect{ subject.top_up 20 }.to change{ subject.balance }.by 20
     end
   end
 end
