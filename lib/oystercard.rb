7f430026710be4e9da62347098d783d3ce0cf4d3
Austin Rowsell <andyrow123@hotmail.co.uk>
2017-10-03 22:09:35 +0100

    Completed challenge 6. Added MAX_BALANCE tests, constant, and code to pass test.

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,11 +1,14 @@
 class Oystercard
   attr_reader :balance
 
+  MAX_BALANCE = 90
+
   def initialize
     @balance = 0
   end
 
   def top_up(amount)
+    fail "Maximum balance of #{ MAX_BALANCE } exceeded" if @balance + amount > MAX_BALANCE
     @balance = @balance + amount
   end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -14,5 +14,11 @@ describe Oystercard do
     it 'should add to balance' do
       expect{ subject.top_up 20 }.to change{ subject.balance }.by 20
     end
+
+    it 'should raise error if exceeds maximum balance' do
+      max_balance = Oystercard::MAX_BALANCE
+      subject.top_up(max_balance)
+      expect{ subject.top_up(1) }.to raise_error "Maximum balance of #{ max_balance } exceeded"
+    end
   end
 end
